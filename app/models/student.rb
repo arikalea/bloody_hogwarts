d322ecc6151f9c38f172a752be8c8e07bb9ae0d7
Arika Turing <ablenker@gmail.com>
2021-02-21 12:55:42 -0700

    Finish user story 3

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,7 @@ class Student < ApplicationRecord
   has_many :student_courses
   has_many :courses, through: :student_courses
 
+  def self.student_count
+   count
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,5 +13,23 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
-  
+  before :each do
+    @student_1 = Student.create!(name: "Casseopia Black", age: 14, house: "Slytherin")
+    @student_2 = Student.create!(name: "AJ Dorion", age: 44, house: "Ravenclaw")
+    # @students = [@student_1, @student_2]
+    @course_1 = Course.create!(name: "Defense against the Dark Arts")
+    @course_2 = Course.create!(name: "Herbology")
+    @course_3 = Course.create!(name: "Potions")
+    # @courses = [@course_1, @course_2, @course_3]
+    @student_course_1 = StudentCourse.create!(course_id: @course_1.id, student_id: @student_1.id)
+    @student_course_2 = StudentCourse.create!(course_id: @course_2.id, student_id: @student_1.id)
+    @student_course_3 = StudentCourse.create!(course_id: @course_3.id, student_id: @student_1.id)
+    @student_course_4 = StudentCourse.create!(course_id: @course_1.id, student_id: @student_2.id)
+  end
+
+  describe 'methods' do
+    it 'counts number of students' do
+      expect(Student.student_count).to eq(2)
+    end
+  end
 end
